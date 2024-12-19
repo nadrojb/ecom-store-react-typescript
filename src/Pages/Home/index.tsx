b6e520071ad00cbab0da15957dd3aa997e1452fa
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-19 16:29:53 +0000

    created fetch for all products

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 
 function Home() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-type products
-
+  type Product = {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+      rate: number;
+      count: number;
+    };
+  };
 
   async function getProducts(setProducts) {
     const json = await fetch(`https://fakestoreapi.com/products`);
-    const productsData = await json.json();
+    const productsData: Product = await json.json();
     setProducts(productsData);
     console.log(products);
-    
   }
 
   useEffect(() => {
     getProducts(setProducts);
   }, []);
- 
 
-  return <>
-  
-  </>;
+  return <></>;
 }
 
 export default Home;
