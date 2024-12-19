6c7adb163a03a162a0c51d412a8f68462d6b898c
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-19 20:26:06 +0000

    sidebar styling complete

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -1,24 +1,39 @@
 function SideBar() {
   return (
     <>
-      <section className="border-r-2 border-slate-300 h-lvh w-10/12 flex justify-center">
-        <div className="text-center mt-16">
+      <section className="h-lvh border-r border-slate-300 w-10/12">
+        <div className=" ml-10 pt-16">
           <img
-            className="w-52 border-b-2 pb-4 border-slate-600"
+            className="w-52 border-b pb-3 border-slate-600"
             src="funstore.png"
             alt=""
           />
-          <div className="flex justify-between mt-5 border-b-2 w-52 pb-5 border-slate-600 text-lg font-medium">
-            <h2 className="border-r-2 border-slate-600 pr-8">
-              Cart <span className="pl-1 text-red-600 font-semibold text-xl">0</span>
+          <div className="flex justify-between mt-3 border-b w-52 pb-3 border-slate-600  ">
+            <h2 className="border-r border-slate-600 pr-8">
+              Cart{" "}
+              <span className="pl-1 text-red-600 ">0</span>
             </h2>
             <h2 className="">$25.00 USD</h2>
           </div>
-          <form>
-            <label htmlFor=""></label>
-            <input type="text" name="" id="" />
-          </form>
         </div>
+          <form className="text mt-4  ml-10">
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="electronics">Electronics</label>
+              <input type="checkbox" name="electronics" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="jewelry">Jewelry</label>
+              <input type="checkbox" name="jewelry" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="mens">Men's clothing</label>
+              <input type="checkbox" name="mens" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="womens">Womens clothing</label>
+              <input type="checkbox" name="womens" id="" />
+            </div>
+          </form>
       </section>
     </>
   );
