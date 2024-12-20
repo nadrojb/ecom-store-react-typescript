786540d82fb71433b24b753bc123ff57345ec145
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-20 13:16:19 +0000

    created route for cart page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </>
