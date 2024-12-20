140a1bbce73600af9e09e4ce118280ad1f4bdc1e
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-20 08:20:12 +0000

    re created side bar on homepage for a simpler version

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -30,15 +30,44 @@ function Home() {
 
   return (
     <>
-      <section className="grid grid-cols-2">
-        <div className="">
-          <SideBar />
-        </div>
-        <div>
-          <h1>products</h1>
-          <h1>products</h1>
-          <h1>products</h1>
-          <h1>products</h1>
+    <section>
+      
+    </section>
+      <section className="grid grid-cols-2 text-slate-800 font-sans">
+        <div className="grid w-4/12 ml-4 mt-16">
+          <img className="w-52" src="leshop.png" alt="" />
+          <div className="border-b border-slate-400 w-52 mt-5"></div>
+          <h2 className="mt-3">
+            Cart <span className="ml-2 bg-red-600 text-white rounded-full px-1 font-semibold text-lg">0</span>
+          </h2>
+          <div className="border-b border-slate-400 w-52 mt-3"></div>
+
+          <form className="mt-3">
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="electronics">
+                Electronics
+              </label>
+              <input type="checkbox" name="electronics" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="jewelry">
+                Jewelry
+              </label>
+              <input type="checkbox" name="jewelry" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="mens">
+                Men's clothing
+              </label>
+              <input type="checkbox" name="mens" id="" />
+            </div>
+            <div className="mt-2">
+              <label className="mr-4" htmlFor="womens">
+                Womens clothing
+              </label>
+              <input type="checkbox" name="womens" id="" />
+            </div>
+          </form>
         </div>
       </section>
     </>
