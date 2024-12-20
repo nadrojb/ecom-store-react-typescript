7ec9e53dccab751f5de6dbebc6502ff3a670356b
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-20 10:37:02 +0000

    product styling complete

--- src/Components/ProductCard/index.tsx
+++ src/Components/ProductCard/index.tsx
@@ -1,11 +1,13 @@
 function ProductCard({ product }) {
   return (
     <>
-      <div>
-        <img src={product.image} alt="" />
+      <div className="border-white bg-white px-2 py-2 rounded-md shadow-md">
+        <div className="h-48 border-whiterounded-md mb-2">
+          <img className="w-24 mx-auto mt-4" src={product.image} alt="" />
+        </div>
         <div className="flex justify-between px-4">
           <h3>{product.price}</h3>
-          <h3 className="">add</h3>
+          <button>add</button>
         </div>
       </div>
     </>
