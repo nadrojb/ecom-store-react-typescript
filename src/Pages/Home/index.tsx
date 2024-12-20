84a32f76bfbd76a624f0ef73119d06c340aa60f2
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-20 08:51:49 +0000

    sidebar styling finished

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -43,18 +43,18 @@ function Home() {
         </div>
       </section>
       <section className="grid grid-cols-2 text-slate-800 font-sans">
-        <div className="grid w-4/12 ml-4 mt-16">
-          <img className="w-52" src="leshop.png" alt="" />
-          <div className="border-b border-slate-400 w-52 mt-5"></div>
-          <h2 className="mt-3">
-            Cart{" "}
-            <span className="ml-2 bg-red-600 text-white rounded-full px-1 font-semibold text-lg">
+        <div className="grid w-3/12 ml-4 mt-16">
+          <img className="w-44" src="leshop.png" alt="" />
+          <div className="border-b border-slate-400 w-40 mt-5"></div>
+          <h2 className="mt-3 text-sm">
+            Cart
+            <span className="ml-2 bg-red-600 text-white rounded-full px-1 font-semibold text-md">
               0
             </span>
           </h2>
-          <div className="border-b border-slate-400 w-52 mt-3"></div>
+          <div className="border-b border-slate-400 w-40 mt-3"></div>
 
-          <form className="mt-3">
+          <form className="mt-3 text-sm">
             <div className="mt-2">
               <label className="mr-4" htmlFor="electronics">
                 Electronics
@@ -80,6 +80,9 @@ function Home() {
               <input type="checkbox" name="womens" id="" />
             </div>
           </form>
+        </div>
+        <div>
+
         </div>
       </section>
     </>
