b297653a9ba85a47e39188fe143521139cde5d73
nadrojb <jordanbrewer1994@gmail.com>
2024-12-28 12:11:00 +0000

    added bin svg to remove item from cart

--- src/Components/CartProductCard/index.tsx
+++ src/Components/CartProductCard/index.tsx
@@ -34,7 +34,11 @@ function CartProductCard({ id, quantity }: CartProductCardProps) {
       </div>
       <div className="flex items-center space-x-4">
         <button
-          onClick={quantity === 1 ? () => removeFromCart(id) : (() => decreaseCartQuantity(id))}
+          onClick={
+            quantity === 1
+              ? () => removeFromCart(id)
+              : () => decreaseCartQuantity(id)
+          }
           className="bg-gray-300 rounded-md px-2 text-md border-gray-300 border-2 hover:bg-white transition duration-100 hover:ease-in"
         >
           -
@@ -46,7 +50,15 @@ function CartProductCard({ id, quantity }: CartProductCardProps) {
         >
           +
         </button>
-        <button onClick={() => removeFromCart(id)}>remove</button>
+
+        <svg
+        onClick={() => removeFromCart(id)}
+          className="w-4 cursor-pointer hover:scale-125 transition hover:ease-in-out"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
+        </svg>
       </div>
     </section>
   );
