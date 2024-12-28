3d3e6144489b67c98f0e1fa32f5edff3fd49172b
nadrojb <jordanbrewer1994@gmail.com>
2024-12-28 09:45:22 +0000

    added custome roboto font to tailwind

--- src/Components/ProductCard/index.tsx
+++ src/Components/ProductCard/index.tsx
@@ -15,7 +15,7 @@ function ProductCard({ product }: Product) {
       <div className="border-white bg-white px-2 py-2 rounded-md shadow-md">
         <div className="h-48 border-white rounded-md mb-2 lg:mb-6">
           <img
-            className="w-24 mx-auto mt-4 lg:w-32 "
+            className="w-24 mx-auto mt-4 lg:w-32"
             src={product.image}
             alt={product.title}
           />
