35f21fa5991b97c5eab98be89b5bc3083e280f9a
nadrojb <jordanbrewer1994@gmail.com>
2024-12-29 08:11:36 +0000

    adjusted the padding on the checkout section

--- src/Pages/Cart/index.tsx
+++ src/Pages/Cart/index.tsx
@@ -15,7 +15,7 @@ function Cart() {
               className="w-7 bg-gray"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
-            >
+              >
               <path d="M459.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-320c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4L288 214.3l0 41.7 0 41.7L459.5 440.6zM256 352l0-96 0-128 0-32c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160C4.2 237.5 0 246.5 0 256s4.2 18.5 11.5 24.6l192 160c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-64z" />
             </svg>
           </Link>
@@ -26,16 +26,16 @@ function Cart() {
       <div>
         {cartItems.map((item) => (
           <CartProductCard
-            key={item.id}
-            id={item.id}
-            quantity={item.quantity}
-            title={item.title}
-            image={item.image}
-            price={item.price}
+          key={item.id}
+          id={item.id}
+          quantity={item.quantity}
+          title={item.title}
+          image={item.image}
+          price={item.price}
           />
         ))}
       </div>
-      <div id="mobile-subtotal-section" className="bg-gray-200 rounded-tl-sm rounded-tr-sm px-4 py-4 fixed bottom-0 w-full">
+      <div className="bg-gray-200 rounded-tl-sm rounded-tr-sm px-4 py-4 fixed bottom-0 w-full sm:px-10 md:px-10 lg:px-10">
         <div className="flex justify-between text-sm sm:text-lg">
           <h4 className="text-gray-600">SUBTOTAL</h4>
           <h4 className="font-semibold">£{FormatPrice(subTotal)}</h4>
