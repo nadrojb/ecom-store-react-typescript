1f8344756e11775d55c238636827eefde9b960bb
nadrojb <jordanbrewer1994@gmail.com>
2024-12-30 15:28:01 +0000

    fixed cart styling

--- src/Components/CartProductCard/index.tsx
+++ src/Components/CartProductCard/index.tsx
@@ -32,33 +32,37 @@ function CartProductCard({ id, quantity }: CartProductCardProps) {
           </p>
         </div>
       </div>
-      <div className="flex items-center space-x-4">
+      <div className="sm:flex">
+
+      <div className="flex space-x-2">
         <button
           onClick={
             quantity === 1
-              ? () => removeFromCart(id)
-              : () => decreaseCartQuantity(id)
+            ? () => removeFromCart(id)
+            : () => decreaseCartQuantity(id)
           }
           className="bg-slate-900 rounded-md w-6 h-6 text-md text-white "
-        >
+          >
           -
         </button>
         <h3>{quantity}</h3>
         <button
           onClick={() => increaseCartQuantity(id)}
-          className="bg-slate-900 rounded-md w-6 h-6 text-md text-white "
-        >
+          className="bg-slate-900 rounded-md w-6 h-6 text-md  text-white "
+          >
           +
         </button>
+        </div>
+        
         <svg
           onClick={() => removeFromCart(id)}
-          className="w-4 cursor-pointer hover:scale-125 transition hover:ease-in-out"
+          className="w-4 mx-auto mt-4 sm:mt-0 sm:ml-2 cursor-pointer hover:scale-125 transition hover:ease-in-out"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 448 512"
-        >
+          >
           <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
         </svg>
-      </div>
+          </div>
     </section>
   );
 }
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -134,7 +134,7 @@ function Home({}) {
             <span className="absolute">
               <svg
                 aria-hidden="true"
-                className=" ml-4 w-6 h-6 text-gray-200 animate-spin dark:text-gray-300 fill-blue-600"
+                className=" ml-4 w-6 h-6 text-gray-200 animate-spin dark:text-gray-300 fill-black"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
