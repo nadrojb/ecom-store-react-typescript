235210af9021e21b2ad5140afcb5e941e71fd316
nadrojb <jordanbrewer1994@gmail.com>
2024-12-30 08:53:21 +0000

    working on homepage header

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -35,7 +35,7 @@ function Home({}) {
 
   return (
     <>
-      <section className="py-5 border-b shadow-md w-full fixed bg-white top-0">
+      <section className="py-5 border-b shadow-md w-full">
         <div className="flex justify justify-between px-8">
           <h1 className="text-xl font-medium">LeShop</h1>
           <div className="flex items-center">
