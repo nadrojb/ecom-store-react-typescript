373251e0b450e26888ce7961c2e12d30c6d6f005
nadrojb <jordanbrewer1994@gmail.com>
2024-12-31 13:16:15 +0000

    finished styling for checkout form

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -1,4 +1,11 @@
+import CartProductCard from "../../Components/CartProductCard";
+import { useCart } from "../../context/CartContext";
+import FormatPrice from "../../Utilities/FormatPrice";
+
 function Checkout() {
+  const { cartItems, subTotal, cartQuantity } = useCart();
+
+
   return (
     <>
       <div className="py-4 border-b border-gray-300">
@@ -6,15 +13,15 @@ function Checkout() {
           LeShop
         </h1>
       </div>
-      <section className="w-full mb-20">
+      <section className="w-full">
         <div className="pt-6">
           <form action="" className="w-10/12 mx-auto">
             <div>
-              <h2 className="text-xl font-medium" htmlFor="">
+              <h2 className="text-xl font-medium">
                 Contact
               </h2>
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 mt-3"
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 mt-3 rounded:md"
                 type="text"
               />
               <label
@@ -25,12 +32,12 @@ function Checkout() {
               </label>
             </div>
             <div>
-              <h2 className="text-xl font-medium" htmlFor="">
+              <h2 className="text-xl font-medium">
                 Delivery
               </h2>
 
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 mt-3"
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 mt-3 rounded:md"
                 type="text"
               />
               <label
@@ -42,7 +49,7 @@ function Checkout() {
             </div>
             <div>
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 "
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 rounded:md "
                 type="text"
               />
               <label
@@ -54,7 +61,7 @@ function Checkout() {
             </div>
             <div>
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 "
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 rounded:md "
                 type="text"
               />
               <label
@@ -66,7 +73,7 @@ function Checkout() {
             </div>
             <div>
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 "
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2  rounded:md"
                 type="text"
               />
               <label
@@ -78,7 +85,7 @@ function Checkout() {
             </div>
             <div>
               <input
-                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 "
+                className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 rounded:md "
                 type="text"
               />
               <label
@@ -93,22 +100,22 @@ function Checkout() {
             <h2 className="text-xl font-medium " htmlFor="">
               Shipping Method
             </h2>
-            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer  w-full">
+            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer  w-full rounded:md">
               <h4 className="text-sm">Next Day</h4>
               <h4 className="font-semibold">£4.99</h4>
             </button>
-            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer w-full">
+            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer w-full rounded:md">
               <h4 className="text-sm">2-3 days</h4>
               <h4 className="font-semibold">£2.49</h4>
             </button>
-            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer w-full">
+            <button className="flex justify-between items-center border border-gray-300 px-2 py-3 mt-3 cursor-pointer w-full rounded-md">
               <h4 className="text-sm">5-7 days</h4>
               <h4 className="font-semibold">£1.99</h4>
             </button>
           </div>
           <div>
             <div className="mt-5 bg-gray-100 w-11/12 mx-auto rounded-md">
-              <div className="w-11/12 mx-auto py-4">
+              <div className="w-11/12 mx-auto py-5">
                 <h2 className="text-xl font-medium mb-3" htmlFor="">
                   Payment
                 </h2>
@@ -118,7 +125,7 @@ function Checkout() {
                 <input
                   placeholder="Card number"
                   type="text"
-                  className="w-full py-3"
+                  className="w-full py-3 border border-gray-300 rounded:md px-2"
                 />
                 <div>
                   <label className="hidden" htmlFor="">
@@ -126,8 +133,8 @@ function Checkout() {
                   </label>
                   <input
                     placeholder="Expiration date (MM / YY)"
-                    type="text" 
-                    className="w-full py-3"
+                    type="text"
+                    className="w-full py-3 mt-3 border border-gray-300 rounded:md px-2"
                   />
                   <label className="hidden" htmlFor="">
                     Security Code
@@ -135,7 +142,7 @@ function Checkout() {
                   <input
                     placeholder="Security code"
                     type="number"
-                    className="w-full py-3"
+                    className="w-full py-3 mt-3 border border-gray-300 rounded:md px-2"
                   />
                 </div>
                 <label className="hidden" htmlFor="">
@@ -144,13 +151,29 @@ function Checkout() {
                 <input
                   placeholder="Name on card"
                   type="text"
-                  className="w-full py-3"
+                  className="w-full py-3 mt-3 border border-gray-300 rounded:md px-2"
                 />
               </div>
             </div>
           </div>
         </div>
-        <div className=""></div>
+      </section>
+      <section className="mt-5 mb-20 w-10/12 mx-auto">
+        <h2 className="text-xl font-medium">Order Summary</h2>
+        <div className="lg:w-9/12">
+            {cartItems.map((item) => (
+              <CartProductCard
+              key={item.id}
+              id={item.id}
+              quantity={item.quantity}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              />
+            ))}
+          </div>
+
+
       </section>
     </>
   );
