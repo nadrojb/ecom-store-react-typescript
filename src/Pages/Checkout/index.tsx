358a4dc7b43907eff960fbdc20e499d591826dcc
nadrojb <jordanbrewer1994@gmail.com>
2024-12-31 09:22:00 +0000

    created layout for form input

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -6,7 +6,13 @@ return (
  </div>
  <section className="w-full">
     <div className="pt-6">
-
+<form action="" className="w-10/12 mx-auto">
+    <div>
+        <h2 className="text-xl font-medium" htmlFor="">Contact</h2>
+        <input className="w-full border border-gray-300 rounded-sm pt-5 pb-1 px-2 mt-3" type="text" />
+        <label className="relative -top-12 left-2 text-sm text-gray-500" htmlFor="">Email</label>
+    </div>
+</form>
     </div>
     <div className="">
 
