67db9485f1191603107bbbdeed23e50d081514dc
nadrojb <jordanbrewer1994@gmail.com>
2025-01-01 20:39:52 +0000

    starting on a new branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecom-store-react-typescript/", 
+  //base: "/ecom-store-react-typescript/", 
 })
