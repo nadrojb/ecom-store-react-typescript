fdaf8c1afead6cbe7436ed408a6fb69b1f8c7821
nadrojb <jordanbrewer1994@gmail.com>
2025-01-04 06:42:26 +0100

    moved form tags so that that form can be submitted properlyy

--- src/Pages/Checkout/index.tsx
+++ src/Pages/Checkout/index.tsx
@@ -8,7 +8,7 @@ function Checkout() {
   const [shippingPrice, setShippingPrice] = useState(0);
   const [shippingOption, setShippingOption] = useState("");
   const [invalidEmail, setInvalidEmail] = useState("");
-
+  const [emailVerificationStatus, setEmailVerificationStatus] = useState(false);
 
   const totalPrice = subTotal + shippingPrice;
 
@@ -22,17 +22,26 @@ function Checkout() {
     validateEmail(value);
   }
 
-
   function validateEmail(email: string) {
     if (
-      email && 
+      email &&
       !email.match(
         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       )
     ) {
       setInvalidEmail("border-red-500 bg-red-50");
+      setEmailVerificationStatus(false);
     } else {
-      setInvalidEmail("");
+      setInvalidEmail("border-green-500 bg-green-50");
+      setEmailVerificationStatus(true);
+    }
+  }
+
+  function handlePaymentSubmission() {
+    if (emailVerificationStatus) {
+      console.log("payment success");
+    } else {
+      console.log("failed");
     }
   }
 
@@ -64,7 +73,8 @@ function Checkout() {
             <div>
               <h2 className="text-xl font-medium">Delivery</h2>
 
-              <input required
+              <input
+                required
                 className="w-full border border-gray-300  pt-5 pb-1 px-2 mt-3 rounded-sm"
                 type="text"
               />
@@ -76,7 +86,8 @@ function Checkout() {
               </label>
             </div>
             <div>
-              <input required
+              <input
+                required
                 className="w-full border border-gray-300  pt-5 pb-1 px-2 rounded-sm "
                 type="text"
               />
@@ -88,7 +99,8 @@ function Checkout() {
               </label>
             </div>
             <div>
-              <input required
+              <input
+                required
                 className="w-full border border-gray-300  pt-5 pb-1 px-2 rounded-sm "
                 type="text"
               />
@@ -100,7 +112,8 @@ function Checkout() {
               </label>
             </div>
             <div>
-              <input required
+              <input
+                required
                 className="w-full border border-gray-300  pt-5 pb-1 px-2  rounded-sm"
                 type="text"
               />
@@ -112,7 +125,8 @@ function Checkout() {
               </label>
             </div>
             <div>
-              <input required
+              <input
+                required
                 className="w-full border border-gray-300  pt-5 pb-1 px-2 rounded-sm "
                 type="text"
               />
@@ -123,82 +137,88 @@ function Checkout() {
                 Postcode *
               </label>
             </div>
-          </form>
-          <div className="w-10/12 mx-auto">
-            <h2 className="text-xl font-medium">Shipping Method</h2>
-            <button
-              onClick={() => handleShippingOption(4.99, "option1")}
-              className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
-                shippingOption === "option1"
-                  ? "border-green-500 bg-green-50"
-                  : "border-gray-300"
-              }`}
-            >
-              <h4 className="text-sm">Next Day</h4>
-              <h4 className="font-semibold">£4.99</h4>
-            </button>
-            <button
-              onClick={() => handleShippingOption(2.49, "option2")}
-              className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
-                shippingOption === "option2"
-                  ? "border-green-500 bg-green-50"
-                  : "border-gray-300"
-              }`}
-            >
-              <h4 className="text-sm">2-3 days</h4>
-              <h4 className="font-semibold">£2.49</h4>
-            </button>
-            <button
-              onClick={() => handleShippingOption(1.99, "option3")}
-              className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
-                shippingOption === "option3"
-                  ? "border-green-500 bg-green-50"
-                  : "border-gray-300"
-              }`}
-            >
-              <h4 className="text-sm">5-7 days</h4>
-              <h4 className="font-semibold">£1.99</h4>
-            </button>
-          </div>
-          <div>
-            <div className="mt-5 bg-gray-100 w-11/12 mx-auto rounded-sm">
-              <div className="w-11/12 mx-auto py-5">
-                <h2 className="text-xl font-medium mb-3">Payment</h2>
-                <label className="hidden">Card number</label>
-                <input
-                  placeholder="Card number"
-                  type="text"
-                  className="w-full py-3 border border-gray-300 rounded-sm px-2"
-                />
-                <div>
+            <div className="w-10/12 mx-auto">
+              <h2 className="text-xl font-medium">Shipping Method</h2>
+              <button
+                onClick={() => handleShippingOption(4.99, "option1")}
+                className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
+                  shippingOption === "option1"
+                    ? "border-green-500 bg-green-50"
+                    : "border-gray-300"
+                }`}
+              >
+                <h4 className="text-sm">Next Day</h4>
+                <h4 className="font-semibold">£4.99</h4>
+              </button>
+              <button
+                onClick={() => handleShippingOption(2.49, "option2")}
+                className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
+                  shippingOption === "option2"
+                    ? "border-green-500 bg-green-50"
+                    : "border-gray-300"
+                }`}
+              >
+                <h4 className="text-sm">2-3 days</h4>
+                <h4 className="font-semibold">£2.49</h4>
+              </button>
+              <button
+                onClick={() => handleShippingOption(1.99, "option3")}
+                className={`flex justify-between items-center border px-2 py-3 mt-3 cursor-pointer w-full rounded-sm ${
+                  shippingOption === "option3"
+                    ? "border-green-500 bg-green-50"
+                    : "border-gray-300"
+                }`}
+              >
+                <h4 className="text-sm">5-7 days</h4>
+                <h4 className="font-semibold">£1.99</h4>
+              </button>
+            </div>
+            <div>
+              <div className="mt-5 bg-gray-100 w-11/12 mx-auto rounded-sm">
+                <div className="w-11/12 mx-auto py-5">
+                  <h2 className="text-xl font-medium mb-3">Payment</h2>
+                  <label className="hidden">Card number</label>
+                  <input
+                    placeholder="Card number"
+                    type="text"
+                    className="w-full py-3 border border-gray-300 rounded-sm px-2"
+                  />
+                  <div>
+                    <label className="hidden" htmlFor="">
+                      Expiration date
+                    </label>
+                    <input
+                      placeholder="Expiration date (MM / YY)"
+                      type="text"
+                      className="w-full py-3 mt-3 border border-gray-300 rounded-sm px-2"
+                    />
+                    <label className="hidden" htmlFor="">
+                      Security Code
+                    </label>
+                    <input
+                      placeholder="Security code"
+                      type="number"
+                      className="w-full py-3 mt-3 border border-gray-300 rounded-sm px-2"
+                    />
+                  </div>
                   <label className="hidden" htmlFor="">
-                    Expiration date
+                    Name on card
                   </label>
                   <input
-                    placeholder="Expiration date (MM / YY)"
+                    placeholder="Name on card"
                     type="text"
                     className="w-full py-3 mt-3 border border-gray-300 rounded-sm px-2"
                   />
-                  <label className="hidden" htmlFor="">
-                    Security Code
-                  </label>
-                  <input
-                    placeholder="Security code"
-                    type="number"
-                    className="w-full py-3 mt-3 border border-gray-300 rounded-sm px-2"
-                  />
                 </div>
-                <label className="hidden" htmlFor="">
-                  Name on card
-                </label>
-                <input
-                  placeholder="Name on card"
-                  type="text"
-                  className="w-full py-3 mt-3 border border-gray-300 rounded-sm px-2"
-                />
               </div>
             </div>
-          </div>
+            <input
+              onSubmit={handlePaymentSubmission}
+              type="submit"
+              value={"Pay Now"}
+              className="w-full text-center text-gray-900 text-xl bg-green-600 rounded-sm h-12 my-4 "
+            />
+          </form>
         </div>
         <section className="mb-20 w-10/12 mx-auto lg:w-5/12 lg:bg-gray-50 lg:h-screen lg:px-10 lg:border-l lg:fixed right-0 top-0 overflow-scroll">
           <h2 className="text-xl font-medium lg:pt-24">Order Summary</h2>
@@ -226,13 +246,7 @@ function Checkout() {
             <h2 className="text-xl font-medium">Total</h2>
             <h2 className="text-xl font-medium">£{FormatPrice(totalPrice)}</h2>
           </div>
-          <div>
-            <input
-              type="submit"
-              value={"Pay Now"}
-              className="w-full text-center text-gray-900 text-xl bg-green-600 rounded-sm h-12 my-4 "
-            />
-          </div>
+          <div></div>
         </section>
       </section>
     </>
